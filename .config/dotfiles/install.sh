dd0bd36deabb378236103d4f4c656150b4cc0735
Jørgen Borgesen <jorgen@nlink.no>
2021-11-17 21:53:52 +0100

    Update install.sh

--- .config/dotfiles/install.sh
+++ .config/dotfiles/install.sh
@@ -6,19 +6,12 @@ shopt -s expand_aliases
 #
 # Add system repositories
 #
-sudo add-apt-repository ppa:peek-developers/stable -y
 
 # Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-# Microsoft vscode
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
 
 # Install system packages
 sudo apt-get update -qq
@@ -33,7 +26,6 @@ sudo apt-get install -qq \
     cloc \
     cmake \
     cmake-curses-gui \
-    code \
     containerd.io \
     curl \
     diodon \
